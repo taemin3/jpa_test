461adf7cfe081f73e4b8b8122011582a8e70647b
taemin Kim <ruhokim1@naver.com>
2024-05-09 23:09:42 +0900

    first commit

---
+++ src/main/java/study5/datajpa5/DataJpa5Application.java
@@ -0,0 +1,13 @@
+package study5.datajpa5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpa5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataJpa5Application.class, args);
+	}
+
+}
---
+++ src/test/java/study5/datajpa5/DataJpa5ApplicationTests.java
@@ -0,0 +1,13 @@
+package study5.datajpa5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpa5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
